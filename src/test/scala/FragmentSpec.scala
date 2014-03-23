344e18c13d718c76c2d9c81b9e5ffe8ce88c1ed8
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-03-23 12:34:35 +0100

    add tests for link and structuredtext fragments

--- src/test/scala/FragmentSpec.scala
+++ src/test/scala/FragmentSpec.scala
@@ -39,9 +39,39 @@ class FragmentSpec extends Specification {
     def query(q: String) = await(api.forms("everything").ref(api.master).query(q).submit())
     val doc = query("""[[:d = at(document.id, "Uyr9_wEAAKYARDMV")]]""").results.head
     "support media" in {
-      doc.pp getLink "test-link.related" must beSome.like {
+      doc getLink "test-link.related" must beSome.like {
         case l: Fragment.MediaLink => l.filename must_== "baastad.pdf"
       }
+    }
+  }
+  "Multiple document link" should {
+    val api = await(Api.get("https://lesbonneschoses.prismic.io/api"))
+    def query(q: String) = await(api.forms("everything").ref(api.master).query(q).submit())
+    val doc = query("""[[:d = at(document.id, "UkL0gMuvzYUANCpi")]]""").results.head
+    "find first link" in {
+      doc getLink "job-offer.location" must beSome.like {
+        case l: Fragment.DocumentLink => l.slug must_== "new-york-fifth-avenue"
+      }
+    }
+    "find all links" in {
+      val links = doc getAll "job-offer.location"
+      links must haveSize(5)
+      links lift 0 must beSome.like {
+        case l: Fragment.DocumentLink => l.slug must_== "new-york-fifth-avenue"
+      }
+      links lift 1 must beSome.like {
+        case l: Fragment.DocumentLink => l.slug must_== "tokyo-roppongi-hills"
+      }
+    }
+  }
+  "StructuredText" should {
+    val api = await(Api.get("https://lesbonneschoses.prismic.io/api"))
+    def query(q: String) = await(api.forms("everything").ref(api.master).query(q).submit())
+    val doc = query("""[[:d = at(document.id, "UkL0gMuvzYUANCpu")]]""").results.head
+    val struct = doc getStructuredText "article.content"
+    println(doc.fragments.keys)
+    "find first" in {
+      struct must beSome
     }
   }
 }
