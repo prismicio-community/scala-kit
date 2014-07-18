b32a760027fa08e62c2a48f18aba6351a449db65
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-07-18 11:44:14 +0200

    scala 2.11 / play 2.3.1

--- project/Build.scala
+++ project/Build.scala
@@ -3,16 +3,16 @@ import Keys._
 
 object BuildSettings {
 
-  val buildName              = "scala-kit"
-  val buildOrganization      = "io.prismic"
-  val buildVersion           = Option(System.getProperty("version")).map(_.trim).getOrElse("1.0-SNAPSHOT")
+  val buildName = "scala-kit"
+  val buildOrganization = "io.prismic"
+  val buildVersion = Option(System.getProperty("version")).map(_.trim).getOrElse("1.0-SNAPSHOT")
+  val buildScalaVersion = "2.11.1"
 
-  val buildSettings = Defaults.defaultSettings ++ Seq (
-    organization    := buildOrganization,
-    version         := buildVersion,
-    scalaVersion    := "2.11.1",
-    crossScalaVersions := Seq("2.11.1", "2.10.4"),
-    scalacOptions   := Seq("-deprecation", "-unchecked", "-feature")
+  val buildSettings = Defaults.defaultSettings ++ Seq(
+    organization := buildOrganization,
+    version := buildVersion,
+    scalaVersion := buildScalaVersion,
+    scalacOptions := Seq("-deprecation", "-unchecked", "-feature")
   )
 }
 
@@ -22,18 +22,18 @@ object KitBuild extends Build {
     BuildSettings.buildName, file("."),
     settings = BuildSettings.buildSettings ++ Seq(
       scalacOptions ++= Seq("-unchecked", "-deprecation", "-feature"),
-      scalacOptions in (Compile, doc) ++= Seq("-doc-root-content", baseDirectory.value+"/root-doc.txt"),
+      scalacOptions in (Compile, doc) ++= Seq("-doc-root-content", baseDirectory.value + "/root-doc.txt"),
 
       resolvers += "Typesafe repository releases" at "http://repo.typesafe.com/typesafe/releases/",
       resolvers += "Sonatype releases" at "http://oss.sonatype.org/content/repositories/releases",
 
       libraryDependencies ++= Seq(
-        "com.typesafe.play"       %% "play-iteratees"      % "2.3.0",
-        "com.typesafe.play"       %% "play-json"           % "2.3.0",
-        "com.typesafe.play"       %% "play-ws"             % "2.3.0",
-        "com.ning"                %  "async-http-client"   % "1.8.9",
-        "commons-collections"     %  "commons-collections" % "3.2.1",
-        "org.specs2" %% "specs2" % "2.3.12" % "test"
+        "com.typesafe.play" %% "play-iteratees" % "2.3.1",
+        "com.typesafe.play" %% "play-json" % "2.3.1",
+        "com.typesafe.play" %% "play-ws" % "2.3.1",
+        "com.ning" % "async-http-client" % "1.8.9",
+        "commons-collections" % "commons-collections" % "3.2.1",
+        "org.specs2" %% "specs2" % "2.3.13" % "test"
       )
     )
   )
