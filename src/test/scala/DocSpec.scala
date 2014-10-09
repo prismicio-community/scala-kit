ddbde6d16ad579e39988dd7db423b5e15fb77a7d
Erwan Loisant <eloisant@gmail.com>
2014-10-09 12:15:58 +0200

    Doc tests

--- src/test/scala/DocSpec.scala
+++ src/test/scala/DocSpec.scala
@@ -1,5 +1,8 @@
 package io.prismic
 
+import java.util.Date
+
+import org.joda.time.DateTime
 import org.specs2.mutable._
 
 import scala.concurrent.duration._
@@ -26,6 +29,43 @@ class DocSpec extends Specification {
       }
       api.refs.size.mustEqual(1)
     }
+    "simple query" in {
+      val resp: Response = await {
+// startgist:ae4378398935f89045bd:prismic-simplequery.scala
+        Api.get("https://lesbonneschoses.prismic.io/api").flatMap { api =>
+          api.forms("everything")
+            .ref(api.master)
+            .query(Predicate.at("document.type", "product")).submit().map { response =>
+            // The response object contains all documents of type "product", paginated
+            response
+          }
+        }
+// endgist
+      }
+      resp.resultsSize.mustEqual(16)
+    }
+
+    "predicates" in {
+      val resp = await {
+// startgist:f1cca71970ad71a4c6ef:prismic-predicates.scala
+        Api.get("https://lesbonneschoses.prismic.io/api").flatMap { api =>
+          api.forms("everything").ref(api.master).query(
+            Predicate.at("document.type", "blog-post"),
+            Predicate.dateAfter("my.blog-post.date", new DateTime(2014, 6, 1, 0, 0))
+          ).submit().map { response =>
+            // All documents of type "product", updated after June 1st, 2014
+            response
+          }
+        }
+// endgist
+      }
+      resp.resultsSize.mustEqual(0)
+    }
+
   }
+
+
+
+
 
 }
