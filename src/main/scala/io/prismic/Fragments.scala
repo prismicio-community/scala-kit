243e592ca5824f38c485cd176405721d70852c9a
Erwan Loisant <eloisant@gmail.com>
2015-03-18 14:14:13 +0100

    Move fragment class to a separate package, as inner class don't appear in the Scaladoc

--- src/main/scala/io/prismic/Fragments.scala
+++ src/main/scala/io/prismic/Fragments.scala
@@ -1,12 +1,11 @@
 package io.prismic
 
-import org.joda.time._
-import spray.json.JsValue
-
 import scala.concurrent._
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.language.{ postfixOps, implicitConversions }
 import scala.util._
+
+import io.prismic.fragments._
 
 trait HtmlSerializer {
   def apply(elt: Fragment.StructuredText.Element, content: String): Option[String]
@@ -30,335 +29,90 @@ object HtmlSerializer {
 
 }
 
-sealed trait Fragment
+trait Fragment
 
 object Fragment {
 
-  sealed trait Link extends Fragment {
-    def getUrl(linkResolver: DocumentLinkResolver): String
-  }
+  @deprecated("Use io.prismic.fragments.Link", "1.3.3")
+  type Link = io.prismic.fragments.Link
 
-  case class WebLink(url: String, contentType: Option[String] = None) extends Link {
-    override def getUrl(linkResolver: DocumentLinkResolver) = url
-    def asHtml(): String = s"""<a href="$url">$url</a>"""
-  }
+  @deprecated("Use io.prismic.fragments.WebLink", "1.3.3")
+  type WebLink = io.prismic.fragments.WebLink
+  @deprecated("Use io.prismic.fragments.WebLink", "1.3.3")
+  val WebLink = io.prismic.fragments.WebLink
 
-  case class MediaLink(url: String, kind: String, size: Long, filename: String) extends Link {
-    override def getUrl(linkResolver: DocumentLinkResolver) = url
-    def asHtml: String = s"""<a href="$url">$filename</a>"""
-  }
+  @deprecated("Use io.prismic.fragments.MediaLink", "1.3.3")
+  type MediaLink = io.prismic.fragments.MediaLink
+  @deprecated("Use io.prismic.fragments.MediaLink", "1.3.3")
+  val MediaLink = io.prismic.fragments.MediaLink
 
-  case class DocumentLink(id: String,
-                          uid: Option[String],
-                          typ: String,
-                          tags: Seq[String],
-                          slug: String,
-                          fragments: Map[String, Fragment],
-                          isBroken: Boolean) extends Link with WithFragments {
-    override def getUrl(linkResolver: DocumentLinkResolver) = linkResolver(this)
-    override def asHtml(linkResolver: DocumentLinkResolver): String = s"""<a href="${linkResolver(this)}">$slug</a>"""
-  }
+  @deprecated("Use io.prismic.fragments.DocumentLink", "1.3.3")
+  type DocumentLink = io.prismic.fragments.DocumentLink
+  @deprecated("Use io.prismic.fragments.DocumentLink", "1.3.3")
+  val DocumentLink = io.prismic.fragments.DocumentLink
 
   // ------------------
 
-  case class Text(value: String) extends Fragment {
-    def asHtml: String = s"""<span class="text">$value</span>"""
-  }
+  @deprecated("Use io.prismic.fragments.Text", "1.3.3")
+  type Text = io.prismic.fragments.Text
+  @deprecated("Use io.prismic.fragments.Text", "1.3.3")
+  val Text = io.prismic.fragments.Text
 
   // ------------------
 
-  case class Date(value: LocalDate) extends Fragment {
-    def asText(pattern: String) = value.toString(pattern)
-    def asHtml: String = s"""<time>$value</time>"""
-  }
+  @deprecated("Use io.prismic.fragments.Date", "1.3.3")
+  type Date = io.prismic.fragments.Date
+  @deprecated("Use io.prismic.fragments.Date", "1.3.3")
+  val Date = io.prismic.fragments.Date
 
-  case class Timestamp(value: DateTime) extends Fragment {
-    def asText(pattern: String) = value.toString(pattern)
-    def asHtml: String = s"""<time>$value</time>"""
-  }
-
-  // ------------------
-
-  case class Number(value: Double) extends Fragment {
-    def asInt = value.toInt
-    def asText(pattern: String) = new java.text.DecimalFormat(pattern).format(value)
-    def asHtml: String = s"""<span class="number">$value</span>"""
-  }
+  @deprecated("Use io.prismic.fragments.Timestamp", "1.3.3")
+  type Timestamp = io.prismic.fragments.Timestamp
+  @deprecated("Use io.prismic.fragments.Timestamp", "1.3.3")
+  val Timestamp = io.prismic.fragments.Timestamp
 
   // ------------------
 
-  case class Color(hex: String) extends Fragment {
-    def asRGB = Color.asRGB(hex)
-    def asHtml: String = s"""<span class="color">$hex</span>"""
-  }
-
-  object Color {
-
-    private val HexColor = """#([a-fA-F0-9]{2})([a-fA-F0-9]{2})([a-fA-F0-9]{2})""".r
-
-    def isValidColorValue(hex: String): Boolean = hex match {
-      case HexColor(r, g, b) => true
-      case _                 => false
-    }
-
-    def asRGB(hex: String): (Int, Int, Int) = hex match {
-      case HexColor(r, g, b) => (Integer.parseInt(r, 16), Integer.parseInt(g, 16), Integer.parseInt(b, 16))
-      case _                 => (0, 0, 0)
-    }
-
-  }
+  @deprecated("Use io.prismic.fragments.Number", "1.3.3")
+  type Number = io.prismic.fragments.Number
+  @deprecated("Use io.prismic.fragments.Number", "1.3.3")
+  val Number = io.prismic.fragments.Number
 
   // ------------------
 
-  case class Embed(typ: String,
-                   provider: Option[String],
-                   url: String,
-                   width: Option[Int],
-                   height: Option[Int],
-                   html: Option[String],
-                   oembedJson: JsValue) extends Fragment {
-    def asHtml(label: Option[String] = None, direction: Option[String] = None): String = {
-      val attributes: Seq[(String, String)] = Seq(
-        ("data-oembed", url),
-        ("data-oembed-type", typ.toLowerCase)
-      ) ++
-        label.map(l => ("class", l)).toSeq ++
-        direction.map(d => ("dir", d)).toSeq ++
-        provider.map(p => ("data-oembed-provider", p.toLowerCase))
-      html.map(html => s"""<div ${attributes.map{case (k, v) => s"""$k="$v""""}.mkString(" ")}>$html</div>""").getOrElse("")
-    }
-  }
+  @deprecated("Use io.prismic.fragments.Color", "1.3.3")
+  type Color = io.prismic.fragments.Color
+  @deprecated("Use io.prismic.fragments.Color", "1.3.3")
+  val Color = io.prismic.fragments.Color
 
   // ------------------
 
-  case class GeoPoint(latitude: Double, longitude: Double) extends Fragment {
-    def asHtml: String = s"""<div class="geopoint"><span class="latitude">${latitude}</span><span class="longitude">${longitude}</span></div>"""
-  }
+  @deprecated("Use io.prismic.fragments.Embed", "1.3.3")
+  type Embed = io.prismic.fragments.Embed
+  @deprecated("Use io.prismic.fragments.Embed", "1.3.3")
+  val Embed = io.prismic.fragments.Embed
 
   // ------------------
 
-  case class Image(main: Image.View, views: Map[String, Image.View] = Map.empty) extends Fragment {
-
-    def getView(key: String): Option[Image.View] = key.toLowerCase match {
-      case "main" => Some(main)
-      case _      => views.get(key)
-    }
-
-    def asHtml: String = main.asHtml
-
-  }
-
-  object Image {
-
-    case class View(url: String, width: Int, height: Int, alt: Option[String]) {
-      def ratio = width / height
-      def asHtml: String = s"""<img alt="${alt.getOrElse("")}" src="$url" width="$width" height="$height" />"""
-    }
-
-  }
-
-  case class Group(docs: Seq[Group.Doc]) extends Fragment {
-
-    def asHtml(linkResolver: DocumentLinkResolver): String =
-      docs map (_ asHtml linkResolver) mkString "\n"
-  }
-
-  object Group {
-
-    case class Doc(fragments: Map[String, Fragment]) extends WithFragments
-
-  }
-
-  case class StructuredText(blocks: Seq[StructuredText.Block]) extends Fragment {
-
-    def getTitle: Option[StructuredText.Block.Heading] = blocks.collectFirst {
-      case h: StructuredText.Block.Heading => h
-    }
-
-    def getFirstParagraph: Option[StructuredText.Block.Paragraph] = blocks.collectFirst {
-      case p: StructuredText.Block.Paragraph => p
-    }
-
-    def getAllParagraphs: Seq[StructuredText.Block.Paragraph] = blocks.collect {
-      case p: StructuredText.Block.Paragraph => p
-    }
-
-    def getFirstImage: Option[StructuredText.Block.Image] = blocks.collectFirst {
-      case i: StructuredText.Block.Image => i
-    }
-
-    def asHtml(linkResolver: DocumentLinkResolver, htmlSerializer: HtmlSerializer = HtmlSerializer.empty): String = {
-      StructuredText.asHtml(blocks, linkResolver, htmlSerializer)
-    }
-
-  }
+  @deprecated("Use io.prismic.fragments.GeoPoint", "1.3.3")
+  type GeoPoint = io.prismic.fragments.GeoPoint
+  @deprecated("Use io.prismic.fragments.GeoPoint", "1.3.3")
+  val GeoPoint = io.prismic.fragments.GeoPoint
 
   // ------------------
 
-  object StructuredText {
+  @deprecated("Use io.prismic.fragments.Image", "1.3.3")
+  type Image = io.prismic.fragments.Image
+  @deprecated("Use io.prismic.fragments.Image", "1.3.3")
+  val Image = io.prismic.fragments.Image
 
-    def asHtml(blocks: Seq[Block], linkResolver: DocumentLinkResolver, htmlSerializer: HtmlSerializer): String = {
-      case class Group(htmlTag: Option[String], blocks: Seq[Block])
+  @deprecated("Use io.prismic.fragments.Group", "1.3.3")
+  type Group = io.prismic.fragments.Group
+  @deprecated("Use io.prismic.fragments.Group", "1.3.3")
+  val Group = io.prismic.fragments.Group
 
-      val grouped: List[Group] = blocks.foldLeft(List.empty[Group]) {
-        case ((group@Group(Some("ul"), _)) :: rest, block@StructuredText.Block.ListItem(_, _, false, _, _)) => group.copy(blocks = group.blocks :+ block) +: rest
-        case ((group@Group(Some("ol"), _)) :: rest, block@StructuredText.Block.ListItem(_, _, true, _, _)) => group.copy(blocks = group.blocks :+ block) +: rest
-        case (groups, block@StructuredText.Block.ListItem(_, _, false, _, _)) => Group(Some("ul"), Seq(block)) +: groups
-        case (groups, block@StructuredText.Block.ListItem(_, _, true, _, _)) => Group(Some("ol"), Seq(block)) +: groups
-        case (groups, block) => Group(None, Seq(block)) +: groups
-      }.reverse
-
-      grouped.flatMap {
-        case Group(Some(tag), bcks) => s"<$tag>" +: bcks.map(block => Block.asHtml(block, linkResolver, htmlSerializer)) :+ s"</$tag>"
-        case Group(None, bcks)      => bcks.map(block => Block.asHtml(block, linkResolver, htmlSerializer))
-      }.mkString("\n\n")
-    }
-
-    private def asHtml(text: String, spans: Seq[Span], linkResolver: DocumentLinkResolver, serializer: HtmlSerializer): String = {
-
-      def escape(character: String): String = {
-        character.replace("<", "&lt;").replace("\n", "<br>")
-      }
-
-      def serialize(element: Element, content: String): String = {
-        serializer(element, content).getOrElse {
-          element match {
-            case b: Block => Block.asHtml(b, linkResolver)
-            case _: Span.Em => s"<em>$content</em>"
-            case _: Span.Strong => s"<strong>$content</strong>"
-            case Span.Hyperlink(_, _, link: DocumentLink) => s"""<a href="${linkResolver(link)}">$content</a>"""
-            case Span.Hyperlink(_, _, link: MediaLink) => s"""<a href="${link.url}">$content</a>"""
-            case Span.Hyperlink(_, _, link: WebLink) => s"""<a href="${link.url}">$content</a>"""
-            case Span.Label(_, _, label) => s"""<span class="$label">$content</span>"""
-            case _ => s"<span>$content</span>"
-          }
-        }
-      }
-
-      case class OpenSpan(span: Span, content: String)
-
-      @scala.annotation.tailrec
-      def step(in: Seq[(Char, Int)], spans: Seq[Span], stack: Seq[OpenSpan] = Nil, html: String = ""): String = {
-        in match {
-          case ((_, pos) :: tail) if stack.headOption.map(_.span.end) == Some(pos) => {
-            // Need to close a tag
-            val tagHtml = serialize(stack.head.span, stack.head.content)
-            stack.drop(1) match {
-              case Nil => step(in, spans, Nil, html + tagHtml)
-              case h :: t => step(in, spans, h.copy(content = h.content + tagHtml) :: t, html)
-            }
-          }
-          case ((_, pos) :: tail) if spans.headOption.map(_.start) == Some(pos) => {
-            // Need to open a tag
-            step(in, spans.drop(1), OpenSpan(spans.head, "") +: stack, html)
-          }
-          case (current, pos) :: tail => {
-            stack match {
-              case Nil =>
-                // Top level
-                step(tail, spans, stack, html + escape(current.toString))
-              case head :: t =>
-                // There is an open span, insert inside
-                step(tail, spans, head.copy(content = head.content + escape(current.toString)) :: t, html)
-            }
-          }
-          case Nil =>
-            stack match {
-              case Nil => html
-              case head :: Nil =>
-                // One last tag open, close it
-                html + serialize(head.span, head.content)
-              case head :: second :: tail =>
-                // At least 2 tags open, close the first and continue
-                step(Nil, spans, second.copy(content = second.content + serialize(head.span, head.content)) :: tail, html)
-            }
-        }
-      }
-      step(text.toList.zipWithIndex, spans.sortWith {
-        case (a, b) if a.start == b.start => (a.end - a.start) > (b.end - b.start)
-        case (a, b) => a.start < b.start
-      })
-    }
-
-    sealed trait Element
-
-    sealed trait Span extends Element {
-      def start: Int
-      def end: Int
-    }
-
-    object Span {
-
-      case class Em(start: Int, end: Int) extends Span
-      case class Strong(start: Int, end: Int) extends Span
-      case class Hyperlink(start: Int, end: Int, link: Link) extends Span
-      case class Label(start: Int, end: Int, label: String) extends Span
-
-    }
-
-    sealed trait Block extends Element {
-      def label: Option[String]
-      def direction: Option[String]
-    }
-
-    object Block {
-
-      sealed trait Text extends Block {
-        def text: String
-        def spans: Seq[Span]
-      }
-
-      object Text {
-        def unapply(t: Text): Option[(String, Seq[Span], Option[String])] = Some(t.text, t.spans, t.label)
-      }
-
-      def asHtml(block: Block, linkResolver: DocumentLinkResolver, htmlSerializer: HtmlSerializer = HtmlSerializer.empty): String = {
-        val cls = (
-          block.label.map(l => s""" class="$l"""").toSeq ++
-          block.direction.map(d => s""" dir="$d"""").toSeq
-        ).mkString(" ")
-        val body = block match {
-          case StructuredText.Block.Text(text, spans, _) => StructuredText.asHtml(text, spans, linkResolver, htmlSerializer)
-          case _ => ""
-        }
-        htmlSerializer(block, body).getOrElse {
-          block match {
-            case StructuredText.Block.Heading(text, spans, level, _, _) => s"""<h$level$cls>$body</h$level>"""
-            case StructuredText.Block.Paragraph(text, spans, _, _) => s"""<p$cls>$body</p>"""
-            case StructuredText.Block.Preformatted(text, spans, _, _) => s"""<pre$cls>$body</pre>"""
-            case StructuredText.Block.ListItem(text, spans, _, _, _) => s"""<li$cls>$body</li>"""
-            case StructuredText.Block.Image(view, hyperlink, label, dir) => {
-              val linkbody = hyperlink match {
-                case Some(link: DocumentLink) => """<a href="$linkResolver(link)">${view.asHtml}</a>"""
-                case Some(link: WebLink) => """<a href="${link.url}">${view.asHtml}</a>"""
-                case Some(link: MediaLink) => """<a href="${link.url}">${view.asHtml}</a>"""
-                case _ => view.asHtml
-              }
-              s"""<p${dir.map(d => s""" dir="$d"""").getOrElse("")} class="${(label.toSeq :+ "block-img").mkString(" ")}">$linkbody</p>"""
-            }
-            case StructuredText.Block.Embed(obj, label, direction) => obj.asHtml(label, direction)
-          }
-        }
-      }
-
-      case class Heading(text: String, spans: Seq[Span], level: Int, label: Option[String], direction: Option[String]) extends Text
-
-      case class Paragraph(text: String, spans: Seq[Span], label: Option[String], direction: Option[String]) extends Text
-
-      case class Preformatted(text: String, spans: Seq[Span], label: Option[String], direction: Option[String]) extends Text
-
-      case class ListItem(text: String, spans: Seq[Span], ordered: Boolean, label: Option[String], direction: Option[String]) extends Text
-
-      case class Image(view: Fragment.Image.View, linkTo: Option[Link], label: Option[String], direction: Option[String]) extends Block {
-        def url = view.url
-        def width = view.width
-        def height = view.height
-      }
-
-      case class Embed(obj: Fragment.Embed, label: Option[String], direction: Option[String]) extends Block
-
-    }
-
-  }
+  @deprecated("Use io.prismic.fragments.StructuredText", "1.3.3")
+  type StructuredText = io.prismic.fragments.StructuredText
+  @deprecated("Use io.prismic.fragments.StructuredText", "1.3.3")
+  val StructuredText = io.prismic.fragments.StructuredText
 
 }
