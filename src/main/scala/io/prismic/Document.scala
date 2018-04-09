c962ee9b67bea1d8de4955ffd003ce4cb2e06675
Sebastien <srenault.contact@gmail.com>
2018-04-09 10:34:54 +0200

    Reformat Document.scala

--- src/main/scala/io/prismic/Document.scala
+++ src/main/scala/io/prismic/Document.scala
@@ -4,20 +4,20 @@ import io.prismic.fragments.{AlternateLanguage, DocumentLink}
 import org.joda.time.DateTime
 
 /**
- * A prismic.io document
- */
+  * A prismic.io document
+  */
 case class Document(
-                     id: String,
-                     uid: Option[String],
-                     typ: String,
-                     href: String,
-                     tags: Seq[String],
-                     slugs: Seq[String],
-                     firstPublicationDate: Option[DateTime],
-                     lastPublicationDate: Option[DateTime],
-                     lang: String,
-                     alternateLanguages: Seq[AlternateLanguage],
-                     fragments: Map[String, Fragment]) extends WithFragments {
+  id: String,
+  uid: Option[String],
+  typ: String,
+  href: String,
+  tags: Seq[String],
+  slugs: Seq[String],
+  firstPublicationDate: Option[DateTime],
+  lastPublicationDate: Option[DateTime],
+  lang: String,
+  alternateLanguages: Seq[AlternateLanguage],
+  fragments: Map[String, Fragment]) extends WithFragments {
 
   def slug: String = slugs.headOption.getOrElse("-")
 
